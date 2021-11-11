9aaf2afdb392513c4c06f7c9635683365a718c01
Vauxc34 <kaxowy12@gmail.com>
2021-11-11 17:25:45 +0100

    first shopex style has been added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,19 +8,19 @@ export const Navbar = ({ totalItems }) => {
 
     const location = useLocation()
 
-    
-
     return (
         
         <div className="navbar-itself">
     
 
             <img src={logo} component={Link} to/>
-            
-            
+            <div className='navigation-options-bar'>
+            <ul>
+                <li><Link className="option -second" to="/sklep">go to home page</Link></li>
+                <li><Link className="option -one" to="/cart">to cart</Link></li>
+            </ul>
+            </div>
             <div className="cart-icon">
-                <Link to="/cart">to cart</Link>
-                <Link to="/sklep">go to home page</Link>
                 <p className="nmbr-items" dangerouslySetInnerHTML={{__html:totalItems}} />
             </div>
             
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -5,16 +5,27 @@ export const Product = ({product, onAddToCart}) => {
     console.log(product)
 
     return (
-        
+        <>
         <div className="item" item={product.id}>
-        <div className="product-container">
         <img className="product-image" /* src={product.image} */ />
+        <div className="product-container">
+        <div className="container-for-desc-item">
         <h1>{product.name}</h1>
         <p dangerouslySetInnerHTML={{__html: product.description }} />
-        <div className="cart-btn" onClick={() => onAddToCart(product.id, 1)}></div>
-        </div> 
+        </div>
+        <div className="container-for-cart-btn">
+
+        <div className="cart-btn" onClick={() => onAddToCart(product.id, 1)}>
+
+        </div>
+
         </div>
         
+        </div>
+         
+        </div>
+        
+        </>
     )
 }
 
